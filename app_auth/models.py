45c0ec76d5c4574074a2c23d65225e54641fab15
lmsandovalm <lmsandoval367@misena.edu.co>
2024-04-06 11:34:00 -0500

    actualizacion

--- app_auth/models.py
+++ app_auth/models.py
@@ -26,12 +26,11 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 class UserProfile(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
 
-
     bio = models.TextField(blank=True)
     avatar = models.ImageField(upload_to='avatars/', blank=True)
 
     def __str__(self):
-        return self.user.email  
+        return f"{self.user.name} - {self.user.email}"
 
 
 ##########################################################
